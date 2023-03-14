836e631b6adb83b36524282336b8572d45f9f1b7
Jose Daniel Perez Mejia <jdaniel10031@outlook.com>
2023-03-14 11:42:16 -0600

    Ahora si el algoritmo de busqueda CRCW

---
+++ BusquedaMinCRCW.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+int main(){
+
+    int L[]={5,1,100,20};
+    int n = (int) sizeof(L)/sizeof(L[0]);
+
+    printf("Arreglo en donde se debe buscar su elemento mínimo \n");
+    mostrarArreglo(L,n);
+    printf("\n");
+    printf("Elemento minimo del arreglo: %i",minCRCW(L,n));
+
+    return 0;
+}
+
+int minCRCW(int L[],int n){
+    int win[n];
+    int indexMin=0;
+
+    #pragma omp parallel for
+    for(int i = 0;i<n;i++){
+        win[i]=0;
+    }
+    printf("Inicializacion del arreglo win con todos sus elementos en 0 \n");
+    mostrarArreglo(win,n);
+    printf("\n");
+
+
+    #pragma omp parallel for
+
+    for(int i =0; i<n;i++){
+        for(int j=i+1; j<n;j++){
+
+                if(L[i]>L[j]){
+                    #pragma omp critical
+                    {
+                    win[i]=1;
+                    }
+                }
+                 else{
+                    #pragma omp critical
+                    {
+                      win[j]=1;
+                    }
+                 }
+
+        }
+    }
+    printf("Comparacion \n");
+    mostrarArreglo(win,n);
+    printf("\n");
+
+    for(int i=0;i<n;i++){
+        if(win[i]==0){
+            indexMin=i;
+        }
+    }
+
+    return (L[indexMin]);
+
+}
+
+void mostrarArreglo(int arr[],int longitud){
+
+    for(int i =0;i<longitud;i++){
+        printf("%i ",arr[i]);
+    }
+}
